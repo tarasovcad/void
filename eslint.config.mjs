658c74f2af39c3235989e5717f8ba1aab3624194
Maksym <tarasovcad@gmail.com>
2026-01-29 19:15:55 -0600

    init: initialize project

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import {defineConfig, globalIgnores} from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import unusedImports from "eslint-plugin-unused-imports";
+import security from "eslint-plugin-security";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+      security: security,
+    },
+    rules: {
+      ...security.configs.recommended.rules,
+      "no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+]);
+
+export default eslintConfig;
